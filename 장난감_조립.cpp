99b00fc9f0c3fa82ebb9f07fcabe762bc9a96b16
Sangmin Yun <dbstkdalsa@naver.com>
2025-02-05 12:07:35 +0900

    2/5 2025 다른 풀이 적용

--- 장난감_조립.cpp
+++ 장난감_조립.cpp
@@ -1,38 +1,81 @@
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+// void dissemble(vector<vector<int>>& v, vector<bool>& completed, int current)
+// {
+//     if (completed[current])
+//     {
+//         return;
+//     }
+
+//     for (int i = 1; i < v[current].size(); ++i)
+//     {
+//         if (v[current][i] == 0 || current == i)
+//         {
+//             continue;
+//         }
+
+//         if (!completed[i])
+//         {
+//             dissemble(v, completed, i);
+//         }
+
+//         int amount = v[current][i];
+//         v[current][i] = 0;
+
+//         for (int j = 1; j < v[i].size(); ++j)
+//         {
+//             v[current][j] += v[i][j] * amount;
+//         }
+//     }
+    
+//     completed[current] = true;
+// }
+
+// int main()
+// {
+//     int n, m;
+
+//     scanf("%d %d", &n, &m);
+
+//     vector<vector<int>> v(n + 1, vector<int>(n + 1, 0));
+//     vector<bool> completed(n + 1, true);
+
+//     for (int i = 0; i < m; ++i)
+//     {
+//         int assembly, part, count;
+//         scanf("%d %d %d", &assembly, &part, &count);
+//         v[assembly][part] = count;
+//         completed[assembly] = false;
+//     }
+
+//     for (int i = 0; i < n; ++i)
+//     {
+//         if (completed[i])
+//         {
+//             v[i][i] = 1;
+//         }
+//     }
+
+//     dissemble(v, completed, n);
+
+//     for (int i = 0; i < v[n].size(); ++i)
+//     {
+//         if (v[n][i] == 0)
+//         {
+//             continue;
+//         }
+
+//         printf("%d %d\n", i, v[n][i]);
+//     }
+// }
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
-
-void dissemble(vector<vector<int>>& v, vector<bool>& completed, int current)
-{
-    if (completed[current])
-    {
-        return;
-    }
-
-    for (int i = 1; i < v[current].size(); ++i)
-    {
-        if (v[current][i] == 0 || current == i)
-        {
-            continue;
-        }
-
-        if (!completed[i])
-        {
-            dissemble(v, completed, i);
-        }
-
-        int amount = v[current][i];
-        v[current][i] = 0;
-
-        for (int j = 1; j < v[i].size(); ++j)
-        {
-            v[current][j] += v[i][j] * amount;
-        }
-    }
-    
-    completed[current] = true;
-}
 
 int main()
 {
@@ -51,16 +94,33 @@ int main()
         completed[assembly] = false;
     }
 
-    for (int i = 0; i < n; ++i)
+    bool flag = true;
+
+    while (flag)
     {
-        if (completed[i])
+        flag = false;
+
+        for (int i = 1; i < n + 1; ++i)
         {
-            v[i][i] = 1;
+            if (completed[i])
+            {
+                continue;
+            }
+
+            if (v[n][i] != 0)
+            {
+                flag = true;
+
+                for (int j = 1; j < n + 1; ++j)
+                {
+                    v[n][j] += v[i][j] * v[n][i];
+                }
+
+                v[n][i] = 0;
+            }
         }
     }
-
-    dissemble(v, completed, n);
-
+    
     for (int i = 0; i < v[n].size(); ++i)
     {
         if (v[n][i] == 0)
